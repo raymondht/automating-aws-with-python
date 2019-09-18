cae1c9d3c5a3e228661324d978aafe15aec24db3
Erayus <raymondhieutran@gmail.com>
2019-09-18 22:11:02 +1000

    [New] Put data into Dynamodb Table

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -19,8 +19,6 @@ def start_label_detection(bucket, key):
         }
         )
 
-    print(response)
-
     return
 
 def get_video_labels(job_id):
@@ -42,13 +40,39 @@ def get_video_labels(job_id):
 
     return response
 
+def make_item(data):
+    if isinstance(data, dict):
+        return { k: make_item(v) for k, v in data.items() }
+
+    if isinstance(data, list):
+        return [ make_item(v) for v in data ]
+
+    if isinstance(data, float):
+        return str(data)
+
+    return data
+
+
 def put_labels_in_db(data, video_name, video_bucket):
-    pass
+    del data['ResponseMetadata']
+    del data['JobStatus']
+
+    data['videoName'] = video_name
+    data['videoBucket'] = video_bucket
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ['DYNAMODB_TABLE_NAME']
+    videos_table = dynamodb.Table(table_name)
+
+    data = make_item(data)
+
+    videos_table.put_item(Item=data)
+
+    return
 
 # Lambda events
 ## Triggered when a new video is uploaded to the created S3 bucket (in serverless.yml) 
 def start_processing_video(event, context):
-    print(event)
     for record in event['Records']:
         bucketName = record['s3']['bucket']['name']
         key = urllib.parse.unquote_plus(record['s3']['object']['key'])
@@ -57,6 +81,8 @@ def start_processing_video(event, context):
             key
         )
     return
+
+
 
 ## Automatically triggered when the notification in start_label_detection is published to the created SNS topic (in serverless.yml)
 def handle_label_detection(event, context):
@@ -67,7 +93,6 @@ def handle_label_detection(event, context):
         s3_bucket = message['Video']['S3Bucket']
 
         response = get_video_labels(job_id)
-        print(response)
         put_labels_in_db(response, s3_object, s3_bucket)
 
     return
